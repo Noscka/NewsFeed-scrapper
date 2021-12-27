c9ad583bfb945a713c099e9a6733e67e41c36c77
Ruski1 <adam882200@gmail.com>
2021-12-27 16:30:17 +0000

    Add project files.

---
+++ NewsFeed/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace NewsFeed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ControlBar_Panel = new System.Windows.Forms.Panel();
+            this.MinimizePictureBox = new System.Windows.Forms.PictureBox();
+            this.ExitPictureBox = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BugReportButton = new System.Windows.Forms.Button();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.Newshowerpanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.ControlBar_Panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MinimizePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExitPictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ControlBar_Panel
+            // 
+            this.ControlBar_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ControlBar_Panel.Controls.Add(this.MinimizePictureBox);
+            this.ControlBar_Panel.Controls.Add(this.ExitPictureBox);
+            this.ControlBar_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ControlBar_Panel.Location = new System.Drawing.Point(4, 4);
+            this.ControlBar_Panel.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.ControlBar_Panel.Name = "ControlBar_Panel";
+            this.ControlBar_Panel.Size = new System.Drawing.Size(1185, 32);
+            this.ControlBar_Panel.TabIndex = 0;
+            this.ControlBar_Panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ControlBar_Panel_MouseDown);
+            // 
+            // MinimizePictureBox
+            // 
+            this.MinimizePictureBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.MinimizePictureBox.Image = global::NewsFeed.Properties.Resources._;
+            this.MinimizePictureBox.Location = new System.Drawing.Point(1121, 0);
+            this.MinimizePictureBox.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.MinimizePictureBox.Name = "MinimizePictureBox";
+            this.MinimizePictureBox.Size = new System.Drawing.Size(32, 32);
+            this.MinimizePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.MinimizePictureBox.TabIndex = 4;
+            this.MinimizePictureBox.TabStop = false;
+            this.MinimizePictureBox.Click += new System.EventHandler(this.MinimizePictureBox_Click);
+            this.MinimizePictureBox.MouseLeave += new System.EventHandler(this.MinimizePictureBox_MouseLeave);
+            this.MinimizePictureBox.MouseHover += new System.EventHandler(this.MinimizePictureBox_MouseHover);
+            // 
+            // ExitPictureBox
+            // 
+            this.ExitPictureBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ExitPictureBox.Image = global::NewsFeed.Properties.Resources.x;
+            this.ExitPictureBox.Location = new System.Drawing.Point(1153, 0);
+            this.ExitPictureBox.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.ExitPictureBox.Name = "ExitPictureBox";
+            this.ExitPictureBox.Size = new System.Drawing.Size(32, 32);
+            this.ExitPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.ExitPictureBox.TabIndex = 5;
+            this.ExitPictureBox.TabStop = false;
+            this.ExitPictureBox.Click += new System.EventHandler(this.ExitPictureBox_Click);
+            this.ExitPictureBox.MouseLeave += new System.EventHandler(this.ExitPictureBox_MouseLeave);
+            this.ExitPictureBox.MouseHover += new System.EventHandler(this.ExitPictureBox_MouseHover);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.panel1.Controls.Add(this.BugReportButton);
+            this.panel1.Controls.Add(this.RefreshButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(4, 605);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1185, 49);
+            this.panel1.TabIndex = 1;
+            // 
+            // BugReportButton
+            // 
+            this.BugReportButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.BugReportButton.Font = new System.Drawing.Font("BankGothic Lt BT", 12F);
+            this.BugReportButton.Location = new System.Drawing.Point(15, 7);
+            this.BugReportButton.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.BugReportButton.Name = "BugReportButton";
+            this.BugReportButton.Size = new System.Drawing.Size(138, 30);
+            this.BugReportButton.TabIndex = 0;
+            this.BugReportButton.Text = "Report Bug";
+            this.BugReportButton.UseVisualStyleBackColor = true;
+            this.BugReportButton.Click += new System.EventHandler(this.BugReportButton_Click);
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.RefreshButton.Font = new System.Drawing.Font("BankGothic Lt BT", 12F);
+            this.RefreshButton.Location = new System.Drawing.Point(525, 7);
+            this.RefreshButton.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(138, 30);
+            this.RefreshButton.TabIndex = 0;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // Newshowerpanel
+            // 
+            this.Newshowerpanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.Newshowerpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Newshowerpanel.Location = new System.Drawing.Point(4, 36);
+            this.Newshowerpanel.Name = "Newshowerpanel";
+            this.Newshowerpanel.Size = new System.Drawing.Size(1185, 569);
+            this.Newshowerpanel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.ClientSize = new System.Drawing.Size(1193, 658);
+            this.Controls.Add(this.Newshowerpanel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ControlBar_Panel);
+            this.Font = new System.Drawing.Font("BankGothic Lt BT", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.MinimumSize = new System.Drawing.Size(500, 450);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(4);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ControlBar_Panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MinimizePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExitPictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ControlBar_Panel;
+        private System.Windows.Forms.PictureBox MinimizePictureBox;
+        private System.Windows.Forms.PictureBox ExitPictureBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.Button BugReportButton;
+        private System.Windows.Forms.FlowLayoutPanel Newshowerpanel;
+    }
+}
+
---
+++ NewsFeed/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Net;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+///using System.Windows.Controls;
+
+namespace NewsFeed
+{
+    public partial class Form1 : Form
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int CS_DROPSHADOW = 0x20000;
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        protected override void WndProc(ref Message m)
+        {
+            const int wmNcHitTest = 0x84;
+            const int htLeft = 10;
+            const int htRight = 11;
+            const int htTop = 12;
+            const int htTopLeft = 13;
+            const int htTopRight = 14;
+            const int htBottom = 15;
+            const int htBottomLeft = 16;
+            const int htBottomRight = 17;
+
+            if (m.Msg == wmNcHitTest)
+            {
+                int x = (int)(m.LParam.ToInt64() & 0xFFFF);
+                int y = (int)((m.LParam.ToInt64() & 0xFFFF0000) >> 16);
+                Point pt = PointToClient(new Point(x, y));
+                Size clientSize = ClientSize;
+                ///allow resize on the lower right corner
+                if (pt.X >= clientSize.Width - 16 && pt.Y >= clientSize.Height - 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(IsMirrored ? htBottomLeft : htBottomRight);
+                    return;
+                }
+                ///allow resize on the lower left corner
+                if (pt.X <= 16 && pt.Y >= clientSize.Height - 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(IsMirrored ? htBottomRight : htBottomLeft);
+                    return;
+                }
+                ///allow resize on the upper right corner
+                if (pt.X <= 16 && pt.Y <= 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(IsMirrored ? htTopRight : htTopLeft);
+                    return;
+                }
+                ///allow resize on the upper left corner
+                if (pt.X >= clientSize.Width - 16 && pt.Y <= 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(IsMirrored ? htTopLeft : htTopRight);
+                    return;
+                }
+                ///allow resize on the top border
+                if (pt.Y <= 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(htTop);
+                    return;
+                }
+                ///allow resize on the bottom border
+                if (pt.Y >= clientSize.Height - 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(htBottom);
+                    return;
+                }
+                ///allow resize on the left border
+                if (pt.X <= 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(htLeft);
+                    return;
+                }
+                ///allow resize on the right border
+                if (pt.X >= clientSize.Width - 16 && clientSize.Height >= 16)
+                {
+                    m.Result = (IntPtr)(htRight);
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ControlBar_Panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        private void ExitPictureBox_MouseHover(object sender, EventArgs e)
+        {
+            ExitPictureBox.BackColor = Color.FromArgb(240, 71, 71);
+        }
+
+        private void ExitPictureBox_MouseLeave(object sender, EventArgs e)
+        {
+            ExitPictureBox.BackColor = Color.FromArgb(45, 45, 48);
+        }
+
+        private void ExitPictureBox_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinimizePictureBox_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void MinimizePictureBox_MouseHover(object sender, EventArgs e)
+        {
+            MinimizePictureBox.BackColor = Color.FromArgb(50, 53, 56);
+        }
+
+        private void MinimizePictureBox_MouseLeave(object sender, EventArgs e)
+        {
+            MinimizePictureBox.BackColor = Color.FromArgb(45, 45, 48);
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            Newshowerpanel.Controls.Clear();
+            Newshowerpanel.AutoScroll = true;
+            using (var client = new WebClient())
+            {
+                string html = client.DownloadString("https://www.bbc.co.uk/");
+
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(html);
+
+                List<string> Titles = new List<string>();
+                List<string> URLS = new List<string>();
+                List<string> Href = new List<string>();
+
+                foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//p[contains(@class, 'PromoHeadline')]"))
+                {
+                    Titles.Add(node.InnerText);
+                }
+                foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//img/@src"))
+                {
+                    URLS.Add(node.GetAttributeValue("src", "nothing"));
+                }
+                foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//a[@href]"))
+                {
+                    Href.Add(node.GetAttributeValue("href", string.Empty));
+                }
+
+                int i = 0;
+                foreach (string b in Titles)
+                {
+                    ///Panel
+                    Panel panel = new Panel();
+                    panel.BackColor = Color.FromArgb(82, 82, 82);
+                    panel.Size = new Size(340, 305);
+                    int y = panel.Location.Y;
+                    int x = panel.Location.X;
+
+                    ///Image Box
+                    byte[] imageData = client.DownloadData(URLS[i]);
+                    MemoryStream stream = new MemoryStream(imageData);
+                    var URLimage = new PictureBox();
+                    URLimage.Image = Image.FromStream(stream);
+                    URLimage.SizeMode = PictureBoxSizeMode.StretchImage;
+                    URLimage.Size = new Size(340, 191);
+                    stream.Close();
+
+                    ///Label Title
+                    var titlelabel = new Label();
+                    titlelabel.Text = b.Replace("#x27;", "'");
+                    titlelabel.Size = new Size(337, 111);
+                    titlelabel.Location = new Point(3, 194);
+
+                    ///Adding to Control
+                    panel.Controls.Add(URLimage);
+                    panel.Controls.Add(titlelabel);
+                    panel.MouseHover += new EventHandler(Panel_MouseHover);
+                    panel.MouseLeave += new EventHandler(Panel_MouseLeave);
+                    panel.Click += new EventHandler(Panel_MouseClick);
+                    panel.Name = i.ToString();
+                    Newshowerpanel.Controls.Add(panel);
+                    i += 1;
+                }
+
+            }
+        }
+
+        private void Panel_MouseClick(object sender, EventArgs e)
+        {
+            Panel senderpanel = (sender as Panel);
+            int idnumber = Int32.Parse(senderpanel.Name.ToString());
+            senderpanel.BackColor = Color.FromArgb(10, 100, 100);
+            ///System.Diagnostics.Process.Start(Href[i]);
+        }
+
+        private void Panel_MouseHover(object sender, EventArgs e)
+        {
+            Panel senderpanel = (sender as Panel);
+            ///senderpanel.BackColor = Color.FromArgb(200, 100, 100);
+        }
+        private void Panel_MouseLeave(object sender, EventArgs e)
+        {
+            Panel senderpanel = (sender as Panel);
+            ///senderpanel.BackColor = Color.FromArgb(82, 82, 82);
+        }
+
+        private void BugReportButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Doesn't Work Yet");
+        }
+    }
+}
